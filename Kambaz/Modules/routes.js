b2879a6360c60f124a3225416c6c53e82bcca74b
Chen Chen <chenchen102259@gmail.com>
2025-08-09 00:41:15 -0700

    fix all

--- Kambaz/Modules/routes.js
+++ Kambaz/Modules/routes.js
@@ -1,20 +1,16 @@
 import * as modulesDao from "./dao.js";
 
 export default function ModuleRoutes(app) {
-  const deleteModule = async (req, res) => {
-    const { moduleId } = req.params;
-    const status = await modulesDao.deleteModule(moduleId);
-    res.send(status);
-  };
+    app.delete("/api/modules/:moduleId", async (req, res) => {
+        const { moduleId } = req.params;
+        const status = await modulesDao.deleteModule(moduleId);
+        res.send(status);
+    });
 
-  const updateModule = async (req, res) => {
-    const { moduleId } = req.params;
-    const moduleUpdates = req.body;
-    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
-    res.send(status);
-  };
-
-  // Both route definitions INSIDE the function
-  app.delete("/api/modules/:moduleId", deleteModule);
-  app.put("/api/modules/:moduleId", updateModule);
+    app.put("/api/modules/:moduleId", async (req, res) => { 
+        const { moduleId } = req.params;
+        const moduleUpdates = req.body;
+        const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+        res.send(status);
+    });
 }
