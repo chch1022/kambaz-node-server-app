9d563067c9148689fe3d341e23ce7e0aa1c98d63
Chen Chen <chenchen102259@gmail.com>
2025-08-09 01:50:11 -0700

    dao.js

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -6,3 +6,30 @@ export function enrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
 }
+
+export function findAllEnrollments() {
+  return Database.enrollments;
+}
+
+export function findEnrollmentsByUserId(userId) {
+  const { enrollments } = Database;
+  return enrollments.filter(enrollment => enrollment.user === userId);
+}
+
+export function findEnrollmentByUserAndCourse(userId, courseId) {
+  const { enrollments } = Database;
+  return enrollments.find(enrollment => 
+    enrollment.user === userId && enrollment.course === courseId
+  );
+}
+
+export function deleteEnrollmentByUserAndCourse(userId, courseId) {
+  const { enrollments } = Database;
+  const index = enrollments.findIndex(enrollment => 
+    enrollment.user === userId && enrollment.course === courseId
+  );
+  if (index !== -1) {
+    return enrollments.splice(index, 1)[0];
+  }
+  return null;
+}
