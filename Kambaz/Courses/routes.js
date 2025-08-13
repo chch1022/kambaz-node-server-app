bfbf7265d566d9dba837e1c8f5782116bf1bd6c7
Chen Chen <chenchen102259@gmail.com>
2025-08-12 17:25:30 -0700

    6.4.2

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,34 +1,46 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 
+
 export default function CourseRoutes(app) {
 
-    app.get("/api/courses", (req, res) => {
-        const courses = dao.findAllCourses();
+    const findAllCourses = async (req, res) => {
+        const courses = await dao.findAllCourses();
         res.send(courses);
-    });
+    }
+    app.get("/api/courses", findAllCourses);
+
+    const createCourse = async (req, res) => {
+        const course = await dao.createCourse(req.body);
+        res.json(course);
+    }
+    app.post("/api/courses", createCourse);
 
 
-    app.delete("/api/courses/:courseId", (req, res) => {
+    const deleteCourse = async (req, res) => {
         const { courseId } = req.params;
-        const status = dao.deleteCourse(courseId);
+        const status = await dao.deleteCourse(courseId);
         res.send(status);
-    });
+    }
+    app.delete("/api/courses/:courseId", deleteCourse);
 
 
-    app.put("/api/courses/:courseId", (req, res) => {
+
+    const updateCourse = async (req, res) => {
         const { courseId } = req.params;
         const courseUpdates = req.body;
-        const status = dao.updateCourse(courseId, courseUpdates);
+        const status = await dao.updateCourse(courseId, courseUpdates);
         res.send(status);
-    });
+    }
+    app.put("/api/courses/:courseId", updateCourse);
 
-    app.get("/api/courses/:courseId/modules", (req, res) => {
+    const findModulesForCourse = async (req, res) => {
         const { courseId } = req.params;
-        const modules = modulesDao.findModulesForCourse(courseId);
+        const modules = await modulesDao.findModulesForCourse(courseId);
         res.json(modules);
-    });
-    
+    }
+    app.get("/api/courses/:courseId/modules", findModulesForCourse);
+
     app.post("/api/courses/:courseId/modules", (req, res) => {
         const { courseId } = req.params;
         const module = {
--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,9 +1,10 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
+
 export function findModulesForCourse(courseId) {
-  const { modules } = Database;
-  return modules.filter((module) => module.course === courseId);
+ return model.find({ course: courseId });
 }
+
 export function createModule(module) {
   const newModule = { ...module, _id: uuidv4() };
   Database.modules = [...Database.modules, newModule];
