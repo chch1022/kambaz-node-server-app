4339714bfa2ad2889fe70eac5ce9a51808989000
Chen Chen <chen.chencc@outlook.com>
2025-08-16 22:40:33 -0700

    added quizRoutes to index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import QuizRoutes from "./Kambaz/Quizzes/routes.js";
 import mongoose from "mongoose"
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
@@ -64,6 +65,7 @@ CourseRoutes(app);
 EnrollmentRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+QuizRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
